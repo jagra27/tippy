18e3522838a128192ce343f8780bd9affa19d9b7
Jada Grant <jagra27@morgan.edu>
2020-01-24 17:47:37 -0500

    Finished tippy tutorial

--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        
+        let tipPrecentages = [0.15, 0.18, 0.2]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPrecentages[tipControl.selectedSegmentIndex]
+        let total = tip + bill
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+        print("Hello")
+        view.endEditing(true)
+    }
 }
 
