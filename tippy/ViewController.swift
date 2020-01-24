45174f7b3d3a2079acab24302715f05216448e67
Jada Grant <jagra27@morgan.edu>
2020-01-24 17:04:49 -0500

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Jada Grant on 1/24/20.
+//  Copyright © 2020 Jada Grant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
